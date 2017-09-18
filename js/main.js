8d04e41a5e2930bdf9d938431b74b9ba3f46a837
Paras-C <paras_c@hotmail.co.uk>
2017-09-18 15:35:12 +0100

    Added collisions, if enemies intersect with the character, gameover is called

--- js/main.js
+++ js/main.js
@@ -101,6 +101,9 @@ function checkBorderC(num){
 function enemies(){
   //Enemy 1
   setInterval(function(){
+    var pos1 = 16;
+    var pos2 = e1;
+    if(currentPosR==pos1&&currentPosC==pos2){gameOver();}
     if($("#16-"+(e1)).css("background-color")=="red"){gameOver();}
     $("#16-"+(e1+1)).css("background-color","transparent");
     $("#16-"+(e1)).css("background-color","green");
@@ -109,10 +112,13 @@ function enemies(){
     {
       e1 = 14;
     }
-  },1000);
+  },800);
 
   //Enemy 2
     setInterval(function(){
+    var pos1 = 15;
+    var pos2 = e2;
+    if(currentPosR==pos1&&currentPosC==pos2){gameOver();}
     if($("#15-"+(e2)).css("background-color")=="red"){gameOver();}
     $("#15-"+(e2+1)).css("background-color","transparent");
     $("#15-"+(e2)).css("background-color","green");
@@ -121,9 +127,12 @@ function enemies(){
     {
       e2 = 14;
     }
-  },600);
+  },500);
   //Enemy 3
     setInterval(function(){
+    var pos1 = 14;
+    var pos2 = e3;
+    if(currentPosR==pos1&&currentPosC==pos2){gameOver();}
     if($("#14-"+(e3)).css("background-color")=="red"){gameOver();}
     $("#14-"+(e3+1)).css("background-color","transparent");
     $("#14-"+(e3)).css("background-color","green");
@@ -132,9 +141,12 @@ function enemies(){
     {
       e3 = 14;
     }
-  },500);
+  },450);
   //Enemy 4
     setInterval(function(){
+    var pos1 = 11;
+    var pos2 = e4;
+    if(currentPosR==pos1&&currentPosC==pos2){gameOver();}
     if($("#11-"+(e4)).css("background-color")=="red"){gameOver();}
     $("#11-"+(e4+1)).css("background-color","transparent");
     $("#11-"+(e4)).css("background-color","green");
@@ -146,6 +158,9 @@ function enemies(){
   },200);
   //Enemy 5
     setInterval(function(){
+    var pos1 = 9;
+    var pos2 = e5;
+    if(currentPosR==pos1&&currentPosC==pos2){gameOver();}
     if($("#9-"+(e5)).css("background-color")=="red"){gameOver();}
     $("#9-"+(e5+1)).css("background-color","transparent");
     $("#9-"+(e5)).css("background-color","green");
@@ -157,6 +172,9 @@ function enemies(){
   },200);
   //Enemy 6
     setInterval(function(){
+    var pos1 = 8;
+    var pos2 = e8;
+    if(currentPosR==pos1&&currentPosC==pos2){gameOver();}      
     if($("#8-"+(e6)).css("background-color")=="red"){gameOver();}
     $("#8-"+(e6+1)).css("background-color","transparent");
     $("#8-"+(e6)).css("background-color","green");
@@ -168,6 +186,9 @@ function enemies(){
   },100);
   //Enemy 7
     setInterval(function(){
+    var pos1 = 7;
+    var pos2 = e7;
+    if(currentPosR==pos1&&currentPosC==pos2){gameOver();}      
     if($("#7-"+(e7)).css("background-color")=="red"){gameOver();}
     $("#7-"+(e7+1)).css("background-color","transparent");
     $("#7-"+(e7)).css("background-color","green");
@@ -179,7 +200,9 @@ function enemies(){
   },150);
   //Enemy 8
     setInterval(function(){
-    if($("#4-"+(e8)).css("background-color")=="red"){gameOver();}
+    var pos1 = 4;
+    var pos2 = e8;
+    if(currentPosR==pos1&&currentPosC==pos2){gameOver();}
     $("#4-"+(e8+1)).css("background-color","transparent");
     $("#4-"+(e8)).css("background-color","green");
     e8--;
@@ -190,6 +213,9 @@ function enemies(){
   },50);
   //Enemy 9
     setInterval(function(){
+    var pos1 = 3;
+    var pos2 = e9;
+    if(currentPosR==pos1&&currentPosC==pos2){gameOver();}
     $("#3-"+(e9+1)).css("background-color","transparent");
     $("#3-"+(e9)).css("background-color","green");
     e9--;
@@ -201,7 +227,7 @@ function enemies(){
 }
 
 function gameOver(){
-  console.log("GAME OVER");
+  alert("GAME OVER");
 }
 
 
