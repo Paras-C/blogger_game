08ab70f144a25dd34110ee3482dff41fa256b6fa
Paras-C <paras_c@hotmail.co.uk>
2017-09-18 15:10:22 +0100

    added enemies that move at varying speeds

--- js/main.js
+++ js/main.js
@@ -3,6 +3,15 @@ var currentPosC;
 
 var prePosR;
 var prePosC;
+var e1=14;
+var e2=14;
+var e3=14;
+var e4=14;
+var e5=14;
+var e6=14;
+var e7=14;
+var e8=14;
+var e9=14;
 $(function(){
 
   drawGame();
@@ -31,15 +40,16 @@ $(function(){
     {
         if(checkBorderC(currentPosC+1)){t = currentPosC+=1;}
         $("#"+s+"-"+t).css("background-color","red");   
-    }
-  });
-
+    }});  
+  enemies();
 });
 
 function characterInit(){
   $("#18-7").css("background-color","red");
   currentPosR=18;
   currentPosC=7;
+  
+
 }
 
 
@@ -88,10 +98,111 @@ function checkBorderC(num){
   }
 }
 
+function enemies(){
+  //Enemy 1
+  setInterval(function(){
+    if($("#16-"+(e1)).css("background-color")=="red"){gameOver();}
+    $("#16-"+(e1+1)).css("background-color","transparent");
+    $("#16-"+(e1)).css("background-color","green");
+    e1--;
+    if(e1<-1)
+    {
+      e1 = 14;
+    }
+  },1000);
 
+  //Enemy 2
+    setInterval(function(){
+    if($("#15-"+(e2)).css("background-color")=="red"){gameOver();}
+    $("#15-"+(e2+1)).css("background-color","transparent");
+    $("#15-"+(e2)).css("background-color","green");
+    e2--;
+    if(e2<-1)
+    {
+      e2 = 14;
+    }
+  },600);
+  //Enemy 3
+    setInterval(function(){
+    if($("#14-"+(e3)).css("background-color")=="red"){gameOver();}
+    $("#14-"+(e3+1)).css("background-color","transparent");
+    $("#14-"+(e3)).css("background-color","green");
+    e3--;
+    if(e3<-1)
+    {
+      e3 = 14;
+    }
+  },500);
+  //Enemy 4
+    setInterval(function(){
+    if($("#11-"+(e4)).css("background-color")=="red"){gameOver();}
+    $("#11-"+(e4+1)).css("background-color","transparent");
+    $("#11-"+(e4)).css("background-color","green");
+    e4--;
+    if(e4<-1)
+    {
+      e4 = 14;
+    }
+  },200);
+  //Enemy 5
+    setInterval(function(){
+    if($("#9-"+(e5)).css("background-color")=="red"){gameOver();}
+    $("#9-"+(e5+1)).css("background-color","transparent");
+    $("#9-"+(e5)).css("background-color","green");
+    e5--;
+    if(e5<-1)
+    {
+      e5 = 14;
+    }
+  },200);
+  //Enemy 6
+    setInterval(function(){
+    if($("#8-"+(e6)).css("background-color")=="red"){gameOver();}
+    $("#8-"+(e6+1)).css("background-color","transparent");
+    $("#8-"+(e6)).css("background-color","green");
+    e6--;
+    if(e6<-1)
+    {
+      e6 = 14;
+    }
+  },100);
+  //Enemy 7
+    setInterval(function(){
+    if($("#7-"+(e7)).css("background-color")=="red"){gameOver();}
+    $("#7-"+(e7+1)).css("background-color","transparent");
+    $("#7-"+(e7)).css("background-color","green");
+    e7--;
+    if(e7<-1)
+    {
+      e7 = 14;
+    }
+  },150);
+  //Enemy 8
+    setInterval(function(){
+    if($("#4-"+(e8)).css("background-color")=="red"){gameOver();}
+    $("#4-"+(e8+1)).css("background-color","transparent");
+    $("#4-"+(e8)).css("background-color","green");
+    e8--;
+    if(e8<-1)
+    {
+      e8 = 14;
+    }
+  },50);
+  //Enemy 9
+    setInterval(function(){
+    $("#3-"+(e9+1)).css("background-color","transparent");
+    $("#3-"+(e9)).css("background-color","green");
+    e9--;
+    if(e9<-1)
+    {
+      e9 = 14;
+    }
+  },130);
+}
 
-
-
+function gameOver(){
+  console.log("GAME OVER");
+}
 
 
 
